27ebad3f9d0f14ca1cbb758eef0778ac10f71529
Caio Vinicius <caiomartins9345@gmail.com>
2026-09-08 19:56:44 -0300

    Modularização do código
    Foi adicionado 3 metodos 
    
    calcularMedia(): Responsável exclusivamente por calcular a média.
    
    verificarSituacao(): Responsável exclusivamente por verificar se o aluno foi aprovado ou reprovado.
    
    apresentarResultados(): Responsável exclusivamente por mostrar as informações na tela.

--- Sistema.java
+++ Sistema.java
@@ -1,17 +1,31 @@
 public class Sistema {
+
     public static void main(String[] args) {
         String nomeAluno = "Carlos";
         double Nota1 = 8;
         double Nota2 = 7;
-        double media = (Nota1 + Nota2) / 2;
 
+        double media = calcularMedia(Nota1, Nota2);
+        String situacao = verificarSituacao(media);
+
+        apresentarResultados(nomeAluno, media, situacao);
+    }
+
+    public static double calcularMedia(double Nota1, double Nota2) {
+        return (Nota1 + Nota2) / 2;
+    }
+
+    public static String verificarSituacao(double media) {
+        if (media >= 6) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+
+    public static void apresentarResultados(String nomeAluno, double media, String situacao) {
         System.out.println("Aluno: " + nomeAluno);
         System.out.println("Media: " + media);
-
-        if (media >= 6) {
-            System.out.println("Aprovado");
-        } else {
-            System.out.println("Reprovado");
-        }
+        System.out.println(situacao);
     }
 }
